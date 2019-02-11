b4d5bf9262899eef0f65e38cca01e9fb4ba9c0e8
ray755 <apriyandim0@gmail.com>
2019-02-11 11:45:26 +0700

    repisitory2

---
+++ latihan4.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int a,b,c,d;
+
+	cout << "masukan nilai pertama : ";
+	cin >>a;
+	cout << "masukan nilai ke-dua : ";
+	cin >>b;
+	cout << "masukan nilai ke-tiga : ";
+	cin >>c;
+	cout << "masukan nilai ke-empat : ";
+	cin >>d;
+
+	if (a>b)
+	{
+		if(a>c)
+			cout << "bilangan terbesar adalah : " << a;
+		else if (a>d)
+			cout << "bilangan terbesar adalah : " << a;
+		else
+			cout << "bilangan terbasar adalah : " << d;
+	}
+
+	else if (b>c)
+	{
+		if(b>d)
+			cout << "bilangan terbesar adalah : " << b;
+		else
+			cout << "bilangan terbesar adalah : " << d;
+	}
+
+	else if (c>d)
+		cout << "bilangan terbesar adalah : " << c;
+
+	else
+		cout << "bilangan terbesar adalah : " << d ;
+
+
+
+	return 0;
+}
